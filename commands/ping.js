f9d954a3e9e7fe86ad15be8854f47a9fdff17170
Kurnia Rizki <gogtempest@gmail.com>
2025-02-01 09:23:26 +0700

    Update the ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,24 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Balas dengan Pong!"),
   async execute(interaction) {
-    await interaction.reply("🏓 Pong!");
+    const clientLatency = Date.now() - message.createdTimestamp; // Hitung latency bot
+        const shardLatency = Math.round(client.ws.ping); // Ping API WebSocket Discord
+
+        // Buat embed yang rapi
+        const pingEmbed = new EmbedBuilder()
+            .setColor('#211F20') // Warna biru Discord
+            .setTitle('🏓 Pong!')
+            .setDescription(
+                `📶 **Client Latency:** \`${clientLatency}ms\`\n` +
+                `💓 **Shard Latency:** \`${shardLatency}ms\``
+            )
+            .setFooter({ text: `Diminta oleh ${message.author.tag}`, iconURL: message.author.displayAvatarURL() })
+            .setTimestamp();
+
+        await message.reply({ embeds: [pingEmbed] });
   },
 };
