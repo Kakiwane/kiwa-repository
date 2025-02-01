1cd3fe712e0e3d7f3fe52a6e4398424a19579ee7
Kurnia Rizki <gogtempest@gmail.com>
2025-02-01 12:06:44 +0700

    Fixed bugs

--- commands/quote.js
+++ commands/quote.js
@@ -2,6 +2,13 @@ module.exports = {
     name: "q",
     description: "Mengulang kembali teks yang diberikan setelah tanda (')",
     async execute(message, args) {
+      const ownerId = "697334077094887428"; // Ganti dengan ID Discord Anda
+
+        if (message.author.id !== ownerId) {
+            return message.reply("🚫 Kamu tidak memiliki izin untuk menggunakan perintah ini!");
+        }
+
+
       if (args.length === 0) {
         return message.reply("❌ Tolong berikan teks untuk dikutip!");
         }
