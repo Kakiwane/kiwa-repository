329271ea49071a8b7c85b3858fc67b10cd495b6b
Kurnia Rizki <gogtempest@gmail.com>
2025-07-29 09:27:03 +0700

    Update

--- commands/announce.js
+++ commands/announce.js
@@ -25,7 +25,7 @@ module.exports = {
 
     if (args.length === 0) {
       message.delete()
-      return message.channel.send("Tolong berikan teks untuk dikutip Boss @697334077094887428!");
+      return message.channel.send("Tolong berikan teks untuk dikutip Boss @Tempest !");
     }
 
     const text = args.join(" ");
