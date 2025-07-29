119cdfcb14adc99371888f08b01f200fea312c77
Kurnia Rizki <gogtempest@gmail.com>
2025-07-29 07:16:26 +0700

    Update & Fix

--- commands/clear.js
+++ commands/clear.js
@@ -6,18 +6,18 @@ module.exports = {
     .setDescription("Menghapus sejumlah pesan di chat")
     .addIntegerOption(option =>
       option.setName("jumlah")
-        .setDescription("Jumlah pesan yang akan dihapus (1-100)")
+        .setDescription("Jumlah pesan yang akan dihapus")
         .setRequired(true)
     )
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
   async execute(interaction) {
     const amount = interaction.options.getInteger("jumlah");
 
-    if (amount < 1 || amount > 100) {
-      // Opsional
-        // const { MessageFlags } = require("discord.js");
-        await interaction.reply({ content: "❌ Kamu tidak memiliki izin!", flags: 64 });        
-    }
+    // if (amount < 1 || amount > 100) {
+    //   // Opsional
+    //     // const { MessageFlags } = require("discord.js");
+    //     await interaction.reply({ content: "❌ Kamu tidak memiliki izin!", flags: 64 });        
+    // }
 
     try {
       const balasan = await interaction.channel.bulkDelete(amount, true);
